642900f0d7c24e2a2fc2ead0cd109172ef0e6f83
Mark <Mark.Fairpo@digit-hull.com>
2018-11-22 17:18:59 +0000

    Added endpoint tests that run the server

--- src/test/java/io/lampada/template/TemplateControllerTests.java
+++ src/test/java/io/lampada/template/TemplateControllerTests.java
@@ -1,5 +1,47 @@
 package io.lampada.template;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+
 public class TemplateControllerTests {
 
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void getShouldReturnNotImplemented() throws Exception {
+        ResponseEntity<String> response = this.restTemplate
+            .getForEntity("http://localhost:" + port + "/", String.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.NOT_IMPLEMENTED);
+    }
+
+    @Test
+    public void postShouldReturnNotImplemented() throws Exception {
+        HttpEntity<Template> request = new HttpEntity<>(new Template());
+        ResponseEntity<String> response = this.restTemplate
+            .postForEntity("http://localhost:" + port + "/", request, String.class);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.NOT_IMPLEMENTED);
+        
+        /*
+        HttpEntity<Foo> request = new HttpEntity<>(new Foo("bar"));
+        URI location = restTemplate.postForLocation(fooResourceUrl, request);
+        assertThat(location, notNullValue());
+        */
+    }
 }

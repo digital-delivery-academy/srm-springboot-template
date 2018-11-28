68df34f87074d49f6a0e10193cc08c1afc044a9b
Alex <alex@alexs-mbp.eduroam.hull.ac.uk>
2018-11-28 13:43:47 +0000

    reformatted some code

--- src/main/java/io/lampada/template/TemplateController.java
+++ src/main/java/io/lampada/template/TemplateController.java
@@ -1,9 +1,5 @@
 package io.lampada.template;
 
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.Instant;
-import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
